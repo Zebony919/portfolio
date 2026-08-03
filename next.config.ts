4a97ebc5122097abea187e052876de2126fb192f
zebony919 <zebony9@gmail.com>
2026-08-03 23:00:28 +1200

    chore: github pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static exports
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support Next.js image optimization
+  },
+
+  basePath: '/portfolio',
 };
 
 export default nextConfig;
